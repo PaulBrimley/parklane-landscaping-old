0b8ef986b2624ca5cfd7eaacd68ec91b0ab8916d
Paul Brimley <paul.brimley@trustalchemy.com>
2021-01-23 08:44:28 -0700

    work on button and some on subscribe now page

--- src/components/Atoms/Button.jsx
+++ src/components/Atoms/Button.jsx
@@ -1,21 +1,109 @@
 import React from 'react';
+import classNames from 'classnames';
 import styled from 'styled-components';
 
 function Button(props) {
-  let classNameArr = ['btn'];
-  if (props.size) classNameArr.push(`btn-${props.size}`);
-  if (props.type) classNameArr.push(props.type);
+  const classes = classNames(props.className, props.classes, {
+    disabled: !!props.disabled
+  });
   return (
-    <StyledButton {...props}>
-      <button className={classNameArr.join(' ')} disabled={props.disabled} onClick={props.onClick}>
-        {props.children}
-      </button>
+    <StyledButton className={classes} {...props} onClick={props.onClick}>
+      {props.children}
     </StyledButton>
   );
 }
 
 const StyledButton = styled.span`
-  .btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: inherit;
+  box-shadow: 2px 2px 2px ${({ theme }) => theme.colorGrey};
+  cursor: pointer;
+  font-size: ${({ fontSize }) => fontSize || 'inherit'};
+  height: ${({ height }) => height || 'auto'};
+  margin: ${({ margin }) => margin || '0 4px'};
+  padding: ${({ padding }) => padding || '6px 14px'};
+  transition: color 0.25s, background 0.25s, border 0.25s, box-shadow 0.25s;
+  width: ${({ width }) => width || 'auto'};
+  white-space: nowrap;
+  &:active {
+    box-shadow: 1px 1px 1px ${({ theme }) => theme.colorGrey};
+  }
+  &.disabled {
+    color: ${({ theme }) => theme.colorGrey};
+    pointer-events: none;
+    background: ${({ theme }) => theme.colorGreyLight};
+    border: 1px solid ${({ theme }) => theme.colorGreyLight};
+    box-shadow: 2px 2px 2px ${({ theme }) => theme.colorGreyMediumLight};
+  }
+  &.btn-xs {
+    padding: 0 2px;
+    font-size: 0.6em;
+  }
+  &.btn-sm {
+    padding: 2px 6px;
+    font-size: 0.8em;
+  }
+  &.btn-md {
+    padding: 2px 6px;
+    font-size: 1em;
+  }
+  &.btn-lg {
+    font-size: 1.2em;
+  }
+  /*&.primary {
+    color: ${({ theme }) => theme.colorWhite};
+    background: ${({ theme }) => theme.colorPrimary};
+    border: 1px solid ${({ theme }) => theme.colorPrimary};
+    &.empty {
+      color: ${({ theme }) => theme.colorPrimary};
+      background: ${({ theme }) => theme.colorTransparent};
+    }
+  }
+  &.primary-hover {
+    &:hover {
+      background: ${({ theme }) => theme.colorPrimaryHover};
+      border-color: ${({ theme }) => theme.colorPrimaryHover};
+    }
+  }
+  &.secondary {
+    color: ${({ theme }) => theme.colorWhite};
+    background: ${({ theme }) => theme.colorSecondary};
+    border: 1px solid ${({ theme }) => theme.colorSecondary};
+    &.empty {
+      color: ${({ theme }) => theme.colorSecondary};
+      background: ${({ theme }) => theme.colorTransparent};
+    }
+  }
+  &.secondary-hover {
+    &:hover {
+      background: ${({ theme }) => theme.colorSecondaryHover};
+      border-color: ${({ theme }) => theme.colorSecondaryHover};
+    }
+  }
+  &.tertiary {
+    color: ${({ theme }) => theme.colorWhite};
+    background: ${({ theme }) => theme.colorTertiary};
+    border: 1px solid ${({ theme }) => theme.colorTertiary};
+    &.empty {
+      color: ${({ theme }) => theme.colorTertiary};
+      background: ${({ theme }) => theme.colorTransparent};
+    }
+  }
+  &.tertiary-hover {
+    &:hover {
+      background: ${({ theme }) => theme.colorTertiaryHover};
+      border-color: ${({ theme }) => theme.colorTertiaryHover};
+    }
+  }
+  &:disabled {
+    pointer-events: none;
+    background: ${({ theme }) => theme.colorGreyLight};
+    border: 1px solid ${({ theme }) => theme.colorGreyMediumLight};
+    box-shadow: 2px 2px 2px ${({ theme }) => theme.colorGreyMediumLight};
+  }*/
+  /*.btn {
     background: inherit;
     //border-radius: 4px;
     border: 1px solid ${({ theme }) => theme.colorWhiteOpaque};
@@ -94,7 +182,7 @@ const StyledButton = styled.span`
         border-color: ${({ theme }) => theme.colorPrimaryHover};
       }
     }
-  }
+  }*/
 `;
 
 export default Button;
--- src/routes/Subscribe.route.jsx
+++ src/routes/Subscribe.route.jsx
@@ -1,7 +1,9 @@
 import { useRef } from 'react';
+import { Textfit } from 'react-textfit';
+import styled from 'styled-components';
 
 /** components **/
-import AnimatedRoute from '../components/Atoms/AnimatedRoute';
+import AnimatedStyledRoute from '../components/Atoms/AnimatedStyledRoute';
 import Button from '../components/Atoms/Button';
 import Footer from '../components/Organisims/Footer';
 import Header from '../components/Organisims/Header';
@@ -10,113 +12,125 @@ import InfoBannerLeft from '../components/Molecules/InfoBannerLeft';
 import PageDivider1 from '../components/Atoms/PageDivider1';
 import StyledInfoBannerAction from '../components/Styled/StyledInfoBannerAction';
 import StyledInfoBannerMessage from '../components/Styled/StyledInfoBannerMessage';
+import StyledInfoBodyMessage from '../components/Styled/StyledInfoBodyMessage';
 
 function SubscribeRoute(props) {
   const scrollRef = useRef();
 
   function handleSubmit() {
-
+    console.log('clicked');
   }
 
   return (
-    <AnimatedRoute ref={scrollRef}>
-      <Header />
-      <div className="body">
-        <InfoBanner
-          backgroundUrl="http://parklanelandscaping.onshark.com/wp-content/uploads/2020/09/slide-newsletter-bkg2.jpg"
-          parallaxStart={140}
-          parallaxStrength={0.2}
-          scrollRef={scrollRef}
-          slotLeft={
-            <InfoBannerLeft
-              /*action={
-                <StyledInfoBannerAction>
-                  <Button onClick={handleSubmit} fontSize="0.8em">LEARN MORE</Button>
-                </StyledInfoBannerAction>
-              }*/
-              message={
-                <StyledInfoBannerMessage
-                  message="The beauty of maintaining HOA community is in the details."
-                  title="THE HOA LANDSCAPE TIPS AND INFO"
-                  titleBolded={['HOA', 'LANDSCAPE']}>
-                  <div className="title">
-                    <div>THE <span className="isBold">HOA</span></div>
-                    <div className="isBold">LANDSCAPE</div>
-                    <div>TIPS AND INFO</div>
-                  </div>
-                  <div className="hr" />
-                  <div className="message">
-                    <div>The beauty of maintaining HOA</div>
-                    <div>community is in the details.</div>
-                  </div>
-                </StyledInfoBannerMessage>
-              }
-            />
-          }
-        />
-        <PageDivider1 />
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-        <p>a bunch of stuff</p>
-      </div>
+    <AnimatedStyledRoute ref={scrollRef}>
+      <StyledSubscribe>
+        <Header />
+        <div className="body">
+          <InfoBanner
+            backgroundUrl="http://parklanelandscaping.onshark.com/wp-content/uploads/2020/09/slide-newsletter-bkg2.jpg"
+            parallaxStart={140}
+            parallaxStrength={0.2}
+            scrollRef={scrollRef}
+            slotLeft={
+              <InfoBannerLeft
+                /*action={
+                  <StyledInfoBannerAction>
+                    <Button onClick={handleSubmit} fontSize="0.8em">LEARN MORE</Button>
+                  </StyledInfoBannerAction>
+                }*/
+                message={
+                  <StyledInfoBannerMessage
+                    message="The beauty of maintaining HOA community is in the details."
+                    title="THE HOA LANDSCAPE TIPS AND INFO"
+                    titleBolded={['HOA', 'LANDSCAPE']}
+                  >
+                    <div className="title">
+                      <div>
+                        THE <span className="isBold">HOA</span>
+                      </div>
+                      <div className="isBold">LANDSCAPE</div>
+                      <div>TIPS AND INFO</div>
+                    </div>
+                    <div className="hr" />
+                    <div className="message">
+                      <div>The beauty of maintaining HOA</div>
+                      <div>community is in the details.</div>
+                    </div>
+                  </StyledInfoBannerMessage>
+                }
+              />
+            }
+          />
+          <PageDivider1 />
+
+          <Textfit className="subscribe-now-header" mode="single">
+            Subscribe now!
+          </Textfit>
+          <Textfit className="receive-emails" mode="single">
+            To receive e-mails with tips and information for your HOA
+          </Textfit>
+
+          <StyledInfoBodyMessage margin="30px">
+            At Parklane Landscaping, our HOA clients are very important to us. Because of this we do our best to stay abreast of
+            the latest information that effects our industry and most importantly the HOA communities we serve. When we come
+            across these little gems or products that we feel are better, more cost-effective, we share the knowledge. Every month
+            these tidbits of information are sent out by Parklane via our subscription email. As a result of this we have been
+            able to help some of our clients save money and add a little more value to their communities. Which at the end of the
+            day is what we are here for. To subscribe, please click the button below.
+          </StyledInfoBodyMessage>
+          <hr className="separator" />
+          <div className="news-letters">
+            <div>news letter 1</div>
+            <div>news letter 2</div>
+          </div>
+          <div className="subscribe-now-action">
+            <Button classes="subscribe-now-button" height="30px" width="150px" fontSize="1.5em" onClick={handleSubmit}>
+              Subscribe Now
+            </Button>
+          </div>
+        </div>
+      </StyledSubscribe>
       <Footer />
-    </AnimatedRoute>
+    </AnimatedStyledRoute>
   );
 }
+const StyledSubscribe = styled.div`
+  .news-letters {
+    display: flex;
+    justify-content: space-around;
+  }
+  .receive-emails {
+    padding: 0 6%;
+    font-weight: 600;
+    color: ${({ theme }) => theme.colorBlack};
+    text-align: center;
+    text-transform: uppercase;
+  }
+  .separator {
+    width: 80%;
+    border: 2px solid red;
+    margin: 20px auto;
+  }
+  .subscribe-now-header {
+    margin-top: 50px;
+    padding: 0 20px;
+    font-weight: 700;
+    color: ${({ theme }) => theme.colorPrimary};
+    text-align: center;
+    text-transform: uppercase;
+  }
+  .subscribe-now-action {
+    display: flex;
+    justify-content: center;
+    .subscribe-now-button {
+      color: ${({ theme }) => theme.colorSecondary};
+      border: 1px solid ${({ theme }) => theme.colorSecondary};
+      &:hover {
+        color: ${({ theme }) => theme.colorWhite};
+        background: ${({ theme }) => theme.colorPrimary};
+        border: 1px solid ${({ theme }) => theme.colorPrimary};
+      }
+    }
+  }
+`;
 export default SubscribeRoute;
