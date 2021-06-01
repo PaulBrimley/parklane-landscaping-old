865e9b73904a68e3eeec8d3f10f5f2125eb28413
paulbrimley <paul.k.brimley@gmail.com>
2021-06-01 08:44:02 -0600

    remove unused component

--- src/components/Styled/StyledPageDivider.jsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-const StyledPageDivider = styled.div`
-  width: ${({ width }) => width || ''};
-  border-bottom: 4px solid ${({ color, theme }) => color || theme.colorPrimary};
-  margin: ${({margin}) => margin || '10px var(--side-margin)'};
-`;
-export default StyledPageDivider;
